4c699b1d911013b3798ffdf7f01c83eaeec3712f
Ngô Đạt <supodatngo123@gmail.com>
2021-02-20 15:10:30 +0700

    modify

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,10 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using API.Data;
+using API.Entity;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var users = _context.Users.Find(id);
+            return users;
+        }
     }
 }
