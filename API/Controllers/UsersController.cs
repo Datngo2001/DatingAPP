8fd1536121635a0f554fd5845b93240c99fa0e0b
Ngô Đạt <supodatngo123@gmail.com>
2021-03-09 12:29:59 +0700

    add account controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,9 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : APIController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
